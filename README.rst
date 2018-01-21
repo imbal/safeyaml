8fd1ce593acc1cf64484c007b9feef95f2817462
tef <tef@twentygototen.org>
2018-01-21 17:42:23 +0000

    README, Parser

---
+++ README.rst
@@ -0,0 +1,126 @@
+SafeYAML
+========
+
+SafeYAML is an aggressively small subset of YAML. It's everything you need for
+human-readable-and-writable configuration files, and nothing more.
+
+You don't need to integrate a new parser library: keep using your language's
+best-maintained YAML parser, and drop the ``safeyaml`` linter into your CI
+pipeline, pre-commit hook and/or text editor. It's a static binary, so you
+don't have any new dependencies to worry about.
+
+
+What's allowed?
+---------------
+
+It's best described as JSON plus the following:
+
+- You can use indentation for structure (braces are optional)
+- Keys can be unquoted (``foo: 1``, rather than ``"foo": 1``)
+- Single-line comments with ``#``
+
+With the following minor YAML features:
+
+- Any JSON/YAML Value can be the root value, not just lists or objects.
+- Trailing commas allowed within `[]` or `{}`.
+- `\xFF`, `\UFFFFFFFF`, along with \uFFFF for escaping code points.
+- A Byte Order Mark codepoint is ignored at the start of a document.
+
+
+Here's an example::
+
+  title: "SafeYAML Example"
+
+  database:
+    server: "192.168.1.1"
+
+    # JSON-style arrays
+    ports: [
+      8000,
+      8001,
+      8002,   # trailing commas are *required* in multi-line structures
+    ]
+
+    enabled: true
+
+  servers:
+    # JSON-style objects
+    alpha: {
+      "ip": "10.0.0.1",
+      "dc": "eqdc10",
+    }
+    beta: {
+      "ip": "10.0.0.2",
+      "dc": "eqdc10",
+    }
+
+As for what's disallowed: a lot. String values must always be quoted. Boolean
+values must be written as ``true`` or ``false`` (``yes``, ``Y``, ``y``, ``on``
+etc are not allowed).
+
+No anchors, no references. No multi-line strings. No multi-document streams. No
+custom tagged values. *No sexagesimal numbers.*
+
+
+Why?
+----
+
+The prevalence of YAML as a configuration format is testament to the
+unfriendliness of JSON, but the YAML language is terrifyingly huge and full of
+pitfalls for people just trying to write configuration (the number of ways to
+write a Boolean value is a prime example).
+
+There have been plenty of attempts to define other configuration languages
+(TOML, Hugo, HJSON, etc). Subjectively, most of them are less friendly than YAML
+(``.ini``-style formats quickly become cumbersome for structures with two or
+more levels of nesting). Objectively, *all* of them face the uphill struggle of
+needing a parser to be written and maintained in every popular programming
+language.
+
+A language which is a subset of YAML, however, needs no new parser - just a
+linter to ensure that files conform. The ``safeyaml`` linter is an independent
+executable, so whatever language and tooling you're currently using, you can
+continue to use it - it's just one more step in your code quality process.
+
+
+How do I use it?
+----------------
+
+The ``safeyaml`` executable will do its best to rewrite your YAML code, or fail
+with an error if it can't. Here's an example of a rewrite, wrapping a string in
+quotes and adding a trailing comma to an array::
+
+  $ cat input.yaml
+  title: My YAML file
+  numbers: [
+    1,
+    4,
+    9
+  ]
+
+  $ safeyaml input.yaml
+  title: "My YAML file"
+  numbers: [
+    1,
+    4,
+    9,
+  ]
+
+By default, the rewritten YAML is printed to standard output. You can pass
+``-w`` to rewrite input files in-place instead.
+
+Here's an example of an error, which must be fixed manually::
+
+  $ cat input.yaml
+  command: yes
+
+  $ safeyaml input.yaml
+  input.yaml:1:9: found unquoted value `yes`, which could be a string or a
+  boolean. Please either surround it in quotes if it's a string, or replace it
+  with `true` if it's a boolean.
+
+
+How do I generate it?
+---------------------
+
+Don't. That's not what YAML is for. Generate JSON if you need to serialize data.
---
+++ safeyaml.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+
+import re
+import io
+import base64
+import json
+
+from collections import namedtuple, OrderedDict
+
+whitespace = re.compile(r"(?:\ |\t|\r|\n|#[^\r\n]*(?:\r?\n|$))+")
+
+int_b10 = re.compile(r"\d[\d_]*")
+
+flt_b10 = re.compile(r"\.[\d_]+")
+exp_b10 = re.compile(r"[eE](?:\+|-)?[\d+_]")
+
+string_dq = re.compile(
+    r'"(?:[^"\\\n\x00-\x1F\uD800-\uDFFF]|\\(?:[\'"\\/bfnrt]|\\\r?\n|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|U[0-9a-fA-F]{8}))*"')
+string_sq = re.compile(
+    r"'(?:[^'\\\n\x00-\x1F\uD800-\uDFFF]|\\(?:[\"'\\/bfnrt]|\\\r?\n|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|U[0-9a-fA-F]{8}))*'")
+
+identifier = re.compile(r"(?!\d)[\w\.]+")
+
+str_escapes = {
+    'b': '\b',
+    'n': '\n',
+    'f': '\f',
+    'r': '\r',
+    't': '\t',
+    '/': '/',
+    '"': '"',
+    "'": "'",
+    '\\': '\\',
+}
+
+escaped = {
+    '\b': '\\b',
+    '\n': '\\n',
+    '\f': '\\f',
+    '\r': '\\r',
+    '\t': '\\t',
+    '"': '\\"',
+    "'": "\\'",
+    '\\': '\\\\',
+}
+
+builtin_names = {'null': None, 'true': True, 'false': False}
+builtin_values = {None: 'null', True: 'true', False: 'false'}
+
+class ParserErr(Exception):
+    def __init__(self, buf, pos, reason=None):
+        self.buf = buf
+        self.pos = pos
+        if reason is None:
+            nl = buf.rfind(' ', pos - 10, pos)
+            if nl < 0:
+                nl = pos - 5
+            reason = "Unknown Character {} (context: {})".format(
+                repr(buf[pos]), repr(buf[pos - 10:pos + 5]))
+        Exception.__init__(self, "{} (at pos={})".format(reason, pos))
+
+
+
+def parse(buf, transform=None):
+    pos = 1 if buf.startswith("\uFEFF") else 0
+    obj, pos = parse_structure(buf, pos, transform)
+
+    m = whitespace.match(buf, pos)
+    if m:
+        pos = m.end()
+        m = whitespace.match(buf, pos)
+
+    if pos != len(buf):
+        raise ParserErr(buf, pos, "Trailing content: {}".format(
+            repr(buf[pos:pos + 10])))
+
+    return obj
+
+
+def parse_structure(buf, pos, transform):
+    return parse_object(buf, pos,transform)
+
+def parse_object(buf, pos, transform=None):
+    m = whitespace.match(buf, pos)
+    if m:
+        pos = m.end()
+
+    peek = buf[pos]
+
+    if peek == '{':
+        out = OrderedDict()
+
+        pos += 1
+        m = whitespace.match(buf, pos)
+        if m:
+            pos = m.end()
+
+        while buf[pos] != '}':
+            key, pos = parse_object(buf, pos, transform)
+
+            if key in out:
+                raise SemanticErr('duplicate key: {}, {}'.format(key, out))
+
+            m = whitespace.match(buf, pos)
+            if m:
+                pos = m.end()
+
+            peek = buf[pos]
+            if peek == ':':
+                pos += 1
+                m = whitespace.match(buf, pos)
+                if m:
+                    pos = m.end()
+            else:
+                raise ParserErr(
+                    buf, pos, "Expected key:value pair but found {}".format(repr(peek)))
+
+            item, pos = parse_object(buf, pos, transform)
+
+            out[key] = item
+
+            peek = buf[pos]
+            if peek == ',':
+                pos += 1
+                m = whitespace.match(buf, pos)
+                if m:
+                    pos = m.end()
+            elif peek != '}':
+                raise ParserErr(
+                    buf, pos, "Expecting a ',', or a '}' but found {}".format(repr(peek)))
+        if transform is not None:
+            out = transform(out)
+        return out, pos + 1
+
+    elif peek == '[':
+        out = []
+
+        pos += 1
+
+        m = whitespace.match(buf, pos)
+        if m:
+            pos = m.end()
+
+        while buf[pos] != ']':
+            item, pos = parse_object(buf, pos, transform)
+            out.append(item)
+
+            m = whitespace.match(buf, pos)
+            if m:
+                pos = m.end()
+
+            peek = buf[pos]
+            if peek == ',':
+                pos += 1
+                m = whitespace.match(buf, pos)
+                if m:
+                    pos = m.end()
+            elif peek != ']':
+                raise ParserErr(
+                    buf, pos, "Expecting a ',', or a ']' but found {}".format(repr(peek)))
+
+        pos += 1
+
+        if transform is not None:
+            out = transform(out)
+        return out, pos
+
+    elif peek == "'" or peek == '"':
+        s = io.StringIO()
+
+        # validate string
+        if peek == "'":
+            m = string_sq.match(buf, pos)
+            if m:
+                end = m.end()
+            else:
+                raise ParserErr(buf, pos, "Invalid single quoted string")
+        else:
+            m = string_dq.match(buf, pos)
+            if m:
+                end = m.end()
+            else:
+                raise ParserErr(buf, pos, "Invalid double quoted string")
+
+        lo = pos + 1  # skip quotes
+        while lo < end - 1:
+            hi = buf.find("\\", lo, end)
+            if hi == -1:
+                if ascii:
+                    s.extend(buf[lo:end - 1].encode('ascii'))
+                else:
+                    s.write(buf[lo:end - 1])  # skip quote
+                break
+
+            if ascii:
+                s.extend(buf[lo:hi].encode('ascii'))
+            else:
+                s.write(buf[lo:hi])
+
+            esc = buf[hi + 1]
+            if esc in str_escapes:
+                if ascii:
+                    s.extend(byte_escapes[esc])
+                else:
+                    s.write(str_escapes[esc])
+                lo = hi + 2
+            elif esc == 'x':
+                n = int(buf[hi + 2:hi + 4], 16)
+                if ascii:
+                    s.append(n)
+                else:
+                    s.write(chr(n))
+                lo = hi + 4
+            elif esc == 'u':
+                n = int(buf[hi + 2:hi + 6], 16)
+                if ascii:
+                    if n > 0xFF:
+                        raise ParserErr(
+                            buf, hi, 'bytestring cannot have escape > 255')
+                    s.append(n)
+                else:
+                    if 0xD800 <= n <= 0xDFFF:
+                        raise ParserErr(
+                            buf, hi, 'string cannot have surrogate pairs')
+                    s.write(chr(n))
+                lo = hi + 6
+            elif esc == 'U':
+                n = int(buf[hi + 2:hi + 10], 16)
+                if ascii:
+                    if n > 0xFF:
+                        raise ParserErr(
+                            buf, hi, 'bytestring cannot have escape > 255')
+                    s.append(n)
+                else:
+                    if 0xD800 <= n <= 0xDFFF:
+                        raise ParserErr(
+                            buf, hi, 'string cannot have surrogate pairs')
+                    s.write(chr(n))
+                lo = hi + 10
+            elif esc == '\n':
+                lo = hi + 2
+            elif (buf[hi + 1:hi + 3] == '\r\n'):
+                lo = hi + 3
+            else:
+                raise ParserErr(
+                    buf, hi, "Unkown escape character {}".format(repr(esc)))
+
+        out = s.getvalue()
+
+        if transform is not None:
+            out = transform(out)
+        return out, end
+
+    elif peek in "-+0123456789":
+
+        flt_end = None
+        exp_end = None
+
+        sign = +1
+
+        if buf[pos] in "+-":
+            if buf[pos] == "-":
+                sign = -1
+            pos += 1
+        peek = buf[pos]
+
+        if peek == '0'
+            raise Exception('Leading Zero')
+
+        m = int_b10.match(buf, pos)
+        if m:
+            int_end = m.end()
+            end = int_end
+        else:
+            raise ParserErr(buf, pos, "Invalid number")
+
+        t = flt_b10.match(buf, end)
+        if t:
+            flt_end = t.end()
+            end = flt_end
+
+        e = exp_b10.match(buf, end)
+        if e:
+            exp_end = e.end()
+            end = exp_end
+
+        if flt_end or exp_end:
+            out = sign * float(buf[pos:end].replace('_', ''))
+        else:
+            out = sign * int(buf[pos:end].replace('_', ''), 10)
+
+        if transform is not None:
+            out = transform(out)
+        return out, end
+
+    else:
+        m = identifier.match(buf, pos)
+        if m:
+            end = m.end()
+            item = buf[pos:end]
+        else:
+            raise ParserErr(buf, pos)
+
+        if item not in builtin_names:
+            raise ParserErr(
+                buf, pos, "{} is not a recognised built-in".format(repr(item)))
+
+        out = builtin_names[item]
+
+        if transform is not None:
+            out = transform(out)
+        return out, end
+
+    raise ParserErr(buf, pos)
+
+
+if __name__ == '__main__':
+    print(parse("123"))
