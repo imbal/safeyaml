3a8878b2d6f4023a807ce0375b1bee4e852ccab2
Aanand Prasad <aanand.prasad@gmail.com>
2018-01-22 16:05:27 +0000

    pytest

--- tests.py
+++ tests.py
@@ -2,92 +2,67 @@ import io
 import os
 import glob
 import yaml
+import pytest
 import safeyaml
 
-def smoke_tests():
-    tests = {
-        """ [0] """:            [0],
-        """ [1.2] """:          [1.2],
-        """ [-3.4] """:         [-3.4],
-        """ [+5.6] """:         [+5.6],
-        """ "test": 1 """:      {'test':1},
-        """ n: 'test' """:      {'n':'test'},
-        """ [1 ,2,3] """:       [1,2,3],
-        """ [1,2,3,] """:       [1,2,3],
-        """ {"a":1} """:        {'a':1},
-        """ {'b':2,} """:     {'b':2},
-        """ [1  #foo\n] """:    [1],
-    }
-    for code, ref_obj in tests.items():
-        try:
-            obj = safeyaml.parse(code)
 
-            if obj != ref_obj:
-                print()
-                print('X', 'smoke test failed: {} should produce {!r}, got {!r}'
-                    .format(code, ref_obj, obj))
-            else:
-                print('.', end='', flush=True)
-        except safeyaml.ParserErr as p:
-            print()
-            print('X',repr(test),"gave",p.name())
+SMOKE_TESTS = {
+    """ [0] """:            [0],
+    """ [1.2] """:          [1.2],
+    """ [-3.4] """:         [-3.4],
+    """ [+5.6] """:         [+5.6],
+    """ "test": 1 """:      {'test':1},
+    """ n: 'test' """:      {'n':'test'},
+    """ [1 ,2,3] """:       [1,2,3],
+    """ [1,2,3,] """:       [1,2,3],
+    """ {"a":1} """:        {'a':1},
+    """ {'b':2,} """:       {'b':2},
+    """ [1  #foo\n] """:    [1],
+}
 
-def test_spec(test):
-    with open(test) as fh:
+
+@pytest.mark.parametrize("code,ref_obj", SMOKE_TESTS.items())
+def test_smoke(code, ref_obj):
+    obj = safeyaml.parse(code)
+    assert obj == ref_obj
+
+
+@pytest.mark.parametrize("path", glob.glob("tests/*.yaml"))
+def test_file(path):
+    with open(path) as fh:
         contents = fh.read()
         try:
             ref_obj = yaml.load(contents)
         except:
-            print()
-            print('X', test, "isn't valid vaml")
-            return
-        
+            raise Exception("input isn't valid YAML: {}".format(contents))
+
         try:
             output = io.StringIO()
             obj = safeyaml.parse(contents, output=output)
             output = output.getvalue()
 
         except safeyaml.ParserErr as p:
-            with open('{}.bad'.format(test)) as fh:
+            with open('{}.bad'.format(path)) as fh:
                 name, pos = fh.readline().split(':')
                 pos = int(pos)
-            if p.name() == name and p.pos == pos:
-                print('.', end='', flush=True)
-                return
-            else:
-                print()
-                print('X',test, "failed, expecting {}:{} got {}:{}".format(name,pos, p.name(), p.pos))
-                return
 
-        if obj != ref_obj:
-            print()
-            print('X', test, 'failed, safeyaml parsed different object from reference parser')
+            assert p.name() == name and p.pos == pos
+            return
+
+        assert obj == ref_obj
 
         try:
             parsed_output = yaml.load(output)
         except Exception as e:
-            print()
-            print('X', test, 'failed, producing invalid YAML as output')
-            return
+            raise Exception("output isn't valid YAML: {}".format(output))
 
-        if parsed_output != ref_obj:
-            print()
-            print('X', test, 'failed, parsed safeyaml output is different from reference parse of input')
-            return
+        assert parsed_output == ref_obj
 
-        with open('{}.ok'.format(test)) as fh:
+        with open('{}.ok'.format(path)) as fh:
             expected_output = fh.read()
 
-        if expected_output != output:
-            print()
-            print('X', test, 'failed. output and {}.ok do not match'.format(test))
-            return
+        assert output == expected_output
 
-        print('.',end='', flush=True)
 
 if __name__ == '__main__':
-    smoke_tests()
-    for spec in glob.glob("tests/*.yaml"):
-        test_spec(spec)
-    print()
-
+    pytest.main(['-q', __file__])
